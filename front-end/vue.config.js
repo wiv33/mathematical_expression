002686ff2abc240810333d1ecce7fcd13106d5b5
PSK <wiv821253@gmail.com>
2020-02-17 22:44:51 +0900

    output custom

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "./docs"),
+  outputDir: path.resolve(__dirname, "../docs"),
+  publicPath: path.resolve("../"),
   transpileDependencies: ["vuetify"]
 };
